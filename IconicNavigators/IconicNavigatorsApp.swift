f4b3752fc8f09d0905cabfd36ae932a5e481d6a1
Zoe Cutler <36316793+zcutlerf@users.noreply.github.com>
2023-09-29 08:37:53 -0400

    Initial Commit

---
+++ IconicNavigators/IconicNavigatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconicNavigatorsApp.swift
+//  IconicNavigators
+//
+//  Created by Zoe Cutler on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct IconicNavigatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
