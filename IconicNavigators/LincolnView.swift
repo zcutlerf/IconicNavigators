698d245d8f8c64a4efb92af4aac88c10c0c8a706
Zoe Cutler <36316793+zcutlerf@users.noreply.github.com>
2023-09-29 08:55:57 -0400

    Added car animation to LincolnView

--- IconicNavigators/LincolnView.swift
+++ IconicNavigators/LincolnView.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct LincolnView: View {
+    @State private var offset1 = -400.0
+    @State private var offset2 = 400.0
+    @State private var offset3 = -800.0
+    
+    private var animation: Animation {
+        .easeIn
+        .speed(0.3)
+        .repeatForever(autoreverses: false)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Image("lincolnnavigator1")
+                    .resizable()
+                    .scaledToFit()
+                    .offset(x: offset1)
+                Image("lincolnnavigator2")
+                    .resizable()
+                    .scaledToFit()
+                    .offset(x: offset2)
+                Image("lincolnnavigator3")
+                    .resizable()
+                    .scaledToFit()
+                    .offset(x: offset3)
+            }
+            .navigationTitle("Lincoln Navigator")
+            .onAppear {
+                withAnimation(animation) {
+                    offset1 += 800.0
+                    offset2 -= 900.0
+                    offset3 += 1400.0
+                }
+            }
+        }
     }
 }
 
